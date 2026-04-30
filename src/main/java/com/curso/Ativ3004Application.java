c6b4136400bbb5636c9e76fa1e80abd5b9136023
DouglasayDF <douglasay2002@gmail.com>
2026-04-30 20:30:47 -0300

    30_04

---
+++ src/main/java/com/curso/Ativ3004Application.java
@@ -0,0 +1,13 @@
+package com.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ativ3004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ativ3004Application.class, args);
+    }
+
+}
